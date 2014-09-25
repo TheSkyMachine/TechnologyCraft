1933f621244c2ae05803cb4ece30ff35589050e5
TheSkyMachine <theskymachine1@gmail.com>
2014-09-25 17:29:32 -0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -8,10 +8,13 @@
 /*----------ITEMS----------*/
 ModPE.setItem(326,"gold_horse_armor",0,"refinedIron",64);
 ModPE.langEdit("item.refinedIron.name","Refined Iron");
-Item.addFurnaceRecipe(265,326,0);
-Item.setCategory(326,ItemCategory.MATERIAL,0);
 
 /*----------BLOCKS----------*/
 Block.defineBlock(23,"Standard Machine","mob_spawner",42,false,0);
- 
+
 Block.defineBlock(25,"Charger",[["mob_spawner",0],["piston_top_normal",0],["piston_top_sticky",0],["piston_top_sticky",0],["piston_top_sticky",0],["piston_top_sticky",0]],17,false,0);
+
+function newLevel()
+{
+  clientMessage("TechnologyCraft: The Revolution");
+}
